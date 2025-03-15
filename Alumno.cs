e5baf47c3bcd9f353e6ccfdca16d5c56340d97a2
Lenin <lenin.030302@gmail.com>
2025-03-14 23:55:30 -0600

    primer commit

---
+++ Alumno.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maestro
+{
+    internal class Alumno
+    {
+        private int n1;
+        private string nombre;
+        private ArrayList materias = new ArrayList();
+        private ArrayList calificaciones = new ArrayList();
+
+        public int N1 { get => n1; set => n1 = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public ArrayList Materias { get => materias; set => materias = value; }
+        public ArrayList Calificaciones { get => calificaciones; set => calificaciones = value; }
+        public void agregarMateria(string materia)
+        {
+            materias.Add(materia);
+        }
+        public void agregarCalificacion(int calificacion)
+        {
+            calificaciones.Add(calificacion);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using maestro;
+
+using System.Data;
+
+Dictionary<string, string> diccionario = new Dictionary<string, string>();
+
+Profesor p = new Profesor();
+Console.WriteLine(" bienvenido ingrese su nombre: ");
+p.Nombre = Console.ReadLine();
+p.capturarDatosAlumno();
+p.capturarMaterias();
+p.capturarCalificaciones();
+
+
+//mostrar datos:
+Console.WriteLine("\nMostrando datos... ");
+Console.WriteLine("************");
+Console.WriteLine("Nombre del profesor: " + p.Nombre);
+Console.WriteLine("\nDatos del alumno: ");
+Console.WriteLine("Numero de lista: " + p.Alumno1.N1);
+Console.WriteLine("Nombre del alumno: " + p.Alumno1.Nombre);
+Console.WriteLine("\nMaterias: ");
+
+for (int i = 0; i < p.Alumno1.Materias.Count; i++)
+{
+   diccionario.Add(p.Alumno1.Materias[i].ToString(), p.Alumno1.Calificaciones[i].ToString());
+}
+
+foreach (var elementos in diccionario)
+{
+    Console.WriteLine(elementos.ToString());
+}
+foreach(string clave in diccionario.Keys)
+{
+    Console.WriteLine(clave);
+}
+foreach (string valor in diccionario.Values)
+{
+    Console.WriteLine(valor);
+}
